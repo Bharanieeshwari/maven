48a1e89a86b87b1093f8e7d15116bc97435410e7
Dishone Prabu J <80575715+dishoneprabu@users.noreply.github.com>
2023-08-11 19:59:18 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
   public String home() {
-    return "Hello Dishone! Your project has built successfully✌️";
+    return "Hello Bharaneeshwari! Your project has been built successfully✌️";
   }
 }
